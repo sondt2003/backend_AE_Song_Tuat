be3a1f089e4b3350bd47002c00bdc976caa6a024
Acer Aspire 7 <sondtph22824@fpt.edu.vn>
2023-09-24 00:23:31 +0700

    'test'

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,7 +3,5 @@ const app = require('../src/app')
 describe('Testing app creation', () => {
     it('App is correctly exported', () => {
         expect(typeof app).toBe('function')
-
-        
     })
 })
