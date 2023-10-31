fe4349ff588c6cc0d6fb2b877fbce99dc696b85c
Đặng Trường Sơn <dangtruongson13032003@gmail.com>
2023-10-31 13:25:40 +0700

    Update

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,8 +1,15 @@
 const { BusinessLogicError } = require('../core/error.response');
 const { category } = require('../models/categories.model');
+const { convert2ObjectId } = require('../utils');
 class CategoryService {
 
     static async createCategory(type, payload) {
+        if(payload.category_parent_id){
+            const foundCategory=await category.findById(convert2ObjectId(payload.category_parent_id)).lean().exec();
+            if(foundCategory.category_parent_id){
+                if (foundCategory.category_parent_id) throw new BusinessLogicError("Category Đã có parent category")
+            }
+        }
         return await category.create({
             ...payload
         });
