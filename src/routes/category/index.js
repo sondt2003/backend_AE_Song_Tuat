f7544c8716312404364b18d52faab68396a38e19
sondt2003vn <116846547+sondt2003vn@users.noreply.github.com>
2023-11-03 00:49:20 +0700

    Update index.js

--- src/routes/category/index.js
+++ src/routes/category/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const categoryController = require('../../controllers/category.controller')
 const {authenticationV2} = require("../../auth/authUtils");
 
-router.use(authenticationV2)
 
-router.post('', categoryController.createCategory)
+
 router.get('', categoryController. getAllCategory)
+
+router.use(authenticationV2)
+router.post('', categoryController.createCategory)
 router.put('/:categoryId', categoryController.updateCategory)
 router.delete('/:categoryId', categoryController.deleteCategory)
 
