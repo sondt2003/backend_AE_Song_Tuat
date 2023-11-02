3459e3709b6cdce85f3a3d26455783971d622441
Đặng Trường Sơn <dangtruongson13032003@gmail.com>
2023-11-02 20:34:09 +0700

    add

--- src/models/shop.model.js
+++ src/models/shop.model.js
@@ -1,44 +1,52 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const DOCUMENT_NAME = 'Shop';
-const COLLECTION_NAME = 'Shops';
+const DOCUMENT_NAME = "Shop";
+const COLLECTION_NAME = "Shops";
 
-const shopSchema = new Schema({
+const shopSchema = new Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-        maxLength: 150
+      type: String,
+      trim: true,
+      maxLength: 150,
+    },
+    avatar: {
+      type: String,
+      trim: true,
+      default:
+        "https://3.bp.blogspot.com/-aFNY0f-9QVg/Ud77oaftMwI/AAAAAAAACkE/QgdoH-rdXu0/s1600/Hinh-Nen-Dep-P1-Sitetin+(20).jpg",
     },
     email: {
-        type: String,
-        unique: true,
-        trim: true
+      type: String,
+      unique: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     msisdn: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'inactive'
+      type: String,
+      enum: ["active", "inactive"],
+      default: "inactive",
     },
     verify: {
-        type: Schema.Types.Boolean,
-        default: false
+      type: Schema.Types.Boolean,
+      default: false,
     },
     roles: {
-        type: Array,
-        default: []
-    }
-}, {
+      type: Array,
+      default: [],
+    },
+  },
+  {
     timestamps: true,
-    collection: COLLECTION_NAME
-});
+    collection: COLLECTION_NAME,
+  }
+);
 
-
-module.exports = model(DOCUMENT_NAME, shopSchema)
+module.exports = model(DOCUMENT_NAME, shopSchema);
