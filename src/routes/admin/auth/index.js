fe48154527a066947477d469a74bfc5f4f4d558b
VueJSKah <vuk19823@gmail.com>
2023-11-17 07:55:47 +0700

    Revert "register and login admin"
    This reverts commit 9635784901311adc9229cf54c1ad634915f83bc7.

--- src/routes/admin/auth/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const controller = require("../../../controllers/admin/access.controller");
-const router = express.Router();
-
-router.post("/register", controller.signUp);
-
-router.post("/login", controller.login);
-module.exports = router;
