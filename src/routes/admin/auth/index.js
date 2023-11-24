2d53e0a1c7b82e2460f6153a7c61a00827b45161
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2023-11-24 23:18:58 +0700

    api create shop for admin

--- src/routes/admin/auth/index.js
+++ src/routes/admin/auth/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const controller = require("../../../controllers/admin/access.controller");
-const router = express.Router();
+
 const validation = require("../../../middleware/validators/access.validator");
+
+const router = express.Router();
 
 router.post("/register", validation.validateLoginRequest, controller.signUp);
 
 router.post("/login", validation.validateLoginRequest, controller.login);
+
+
 module.exports = router;
--- src/routes/admin/statistical/index.js
+++ src/routes/admin/statistical/index.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 router.use("/user", require("./user"));
 router.use("/product", require("./product"));
-
+router.use("/shop",require("./shop"))
 module.exports = router;
