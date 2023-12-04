567740aeb58f8630f8a7b4ad0db84479981fddfb
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2023-12-04 21:46:10 +0700

    create transaction history when nạp tiền

--- src/services/shop/wallet.service.js
+++ src/services/shop/wallet.service.js
@@ -1,83 +1,89 @@
 const Wallet = require("../../models/wallet.model");
 const Shop = require("../../models/shop.model");
 const {
-  Api404Error,
-  Api409Error,
-  Api401Error,
+    Api404Error,
+    Api409Error,
+    Api401Error,
 } = require("../../core/error.response");
+const {TransactionHistoryService} = require("./transaction-history.service");
+
 
 class WalletService {
-  static async Depositing(userId, amount) {
-    try {
-      const shop = await Shop.findOne({ _id: userId });
+    static async Depositing(userId, amount) {
+        try {
+            const shop = await Shop.findOne({_id: userId});
 
-      if (!shop) {
-        throw new Api404Error("Shop not found");
-      }
+            if (!shop) {
+                throw new Api404Error("Shop not found");
+            }
+            const wallet = await Wallet.findOneAndUpdate(
+                {_id: shop.wallet},
+                {$inc: {balance: amount}},
+                {new: true}
+            );
+            if (wallet) {
+                await TransactionHistoryService.createUserTransactionHistory({
+                    payment_method: 'bank_transfer',
+                    currency: amount,
+                    userId: userId,
+                    comments: 'Nạp tiền từ ngân hàng',
+                })
 
-      const wallet = await Wallet.findOneAndUpdate(
-        { _id: shop.wallet },
-        { $inc: { balance: amount } },
-        { new: true }
-      );
+            }
 
-      return wallet;
-    } catch (error) {
-      console.error("Error depositing amount:", error);
-      throw new Api404Error("Failed to deposit amount");
+            return wallet;
+        } catch (error) {
+            console.error("Error depositing amount:", error);
+            throw new Api404Error("Failed to deposit amount");
+        }
     }
-  }
 
-  static async Payoff(userId, amount) {
-    try {
-      const shop = await Shop.findOne({ _id: userId });
+    static async Payoff(userId, amount) {
+        try {
+            const shop = await Shop.findOne({_id: userId});
 
-      if (!shop) {
-        throw new Api404Error("Shop not found");
-      }
+            if (!shop) {
+                throw new Api404Error("Shop not found");
+            }
 
-      const wallet = await Wallet.findOneAndUpdate(
-        { _id: shop.wallet, balance: { $gte: amount } },
-        { $inc: { balance: -amount } },
-        { new: true }
-      );
+            const wallet = await Wallet.findOneAndUpdate(
+                {_id: shop.wallet, balance: {$gte: amount}},
+                {$inc: {balance: -amount}},
+                {new: true}
+            );
 
-      if (!wallet) {
-        throw new Api404Error("Insufficient balance for payoff");
-      }
+            if (!wallet) {
+                throw new Api404Error("Insufficient balance for payoff");
+            }
 
-      return wallet;
-    } catch (error) {
-      console.error("Error paying off amount:", error);
-      throw new Api404Error("Failed to pay off amount");
+            return wallet;
+        } catch (error) {
+            console.error("Error paying off amount:", error);
+            throw new Api404Error("Failed to pay off amount");
+        }
     }
-  }
-  static async GetBalance(userId) {
-    try {
-      const shop = await Shop.findOne({ _id: userId }).populate("wallet");
 
-      if (!shop) {
-        // Handle the case where the shop is not found
-        throw new Api404Error("Shop not found");
-      }
-
-      if (!shop.wallet) {
-        // If wallet not found, create a new wallet and link it to the shop
-        const newWallet = await Wallet.create({ balance: 0, transactions: [], currency: "VND" });
-        shop.wallet = newWallet._id;
-        await shop.save();
-                
-        return newWallet;
-      }
-
-      return shop.wallet;
-    } catch (error) {
-      // Handle any errors that might occur while getting the balance
-      console.error("Error getting balance:", error);
-      throw new Api409Error("Failed to get balance");
-     
+    static async GetBalance(userId) {
+        try {
+            const shop = await Shop.findOne({_id: userId}).populate("wallet");
+            if (!shop) {
+                // Handle the case where the shop is not found
+                throw new Api404Error("Shop not found");
+            }
+            if (!shop.wallet) {
+                // If wallet not found, create a new wallet and link it to the shop
+                const newWallet = await Wallet.create({balance: 0, transactions: [], currency: "VND"});
+                shop.wallet = newWallet._id;
+                await shop.save();
+                return newWallet;
+            }
+            return shop.wallet;
+        } catch (error) {
+            // Handle any errors that might occur while getting the balance
+            console.error("Error getting balance:", error);
+            throw new Api409Error("Failed to get balance");
+        }
     }
-  }
 }
 
 module.exports = WalletService;
