4fbd975079a5017d096d2122bb7a6a911161b655
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2023-12-04 15:37:25 +0700

    Update category.service.js

--- src/services/shop/category.service.js
+++ src/services/shop/category.service.js
@@ -5,12 +5,12 @@ const {convert2ObjectId} = require('../../utils');
 class CategoryService {
 
     static async createCategory(type, payload) {
-        if (payload.category_parent_id) {
-            const foundCategory = await category.findById(convert2ObjectId(payload.category_parent_id)).lean().exec();
-            if (foundCategory.category_parent_id) {
-                if (foundCategory.category_parent_id) throw new BusinessLogicError("Category Đã có parent category")
-            }
-        }
+        // if (payload.category_parent_id) {
+        //     const foundCategory = await category.findById(convert2ObjectId(payload.category_parent_id)).lean().exec();
+        //     if (foundCategory.category_parent_id) {
+        //         if (foundCategory.category_parent_id) throw new BusinessLogicError("Category Đã có parent category")
+        //     }
+        // }
         return await category.create({
             ...payload
         });
@@ -28,17 +28,12 @@ class CategoryService {
     }
 
     static async updateCategory(type, categoryId, payload) {
-        const categoryClass = CategoryService.categoryRegistry[type]
-        if (!categoryClass) throw new BusinessLogicError("Category type %s không hợp lệ")
         return category.findByIdAndUpdate(categoryId, {
             ...payload
         }, {new: true}).lean();
     }
 
     static async deleteCategory(categoryId) {
-        const categoryClass = CategoryService.categoryRegistry[type]
-        if (!categoryClass) throw new BusinessLogicError("Category type %s không hợp lệ")
-
         return category.findByIdAndDelete(categoryId, {new: true}).lean();
     }
 }
